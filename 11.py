46fd5e80674dbba9c2f4f88558a90c90673ae6de
root <root@mail.linuxprobe.com>
2016-10-06 22:19:57 +0800

    python学习之switch，参数冗余

---
+++ 11.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#!/coding:utf-8
+
+def fun(x):
+         print x
+
+
+def machine(x=5,y='奶油'):
+    print "生成一个" ,x,'元',y,'口味的冰淇淋'
+
+#machine(5,'巧克力')
+machine()
+
+#s = raw_input("input something:")
+#fun(s)
---
+++ scal.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding:utf-8
+
+#调用模块，使除法精确小数位
+from __future__  import division
+
+def jia(x,y):
+    return x+y
+
+def jian(x,y):
+    return x-y
+
+def cheng(x,y):
+    return x*y
+
+def chu(x,y):
+    return x/y
+
+operator = {"+":jia,"-":jian,"*":cheng,"/":chu}
+
+def f(x,o,y):
+    print operator.get(o)(x,y)
+
+f(3,"+",2)

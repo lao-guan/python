f7098a8cc63283e85c99543aee1ca6fd2ca25bc8
root <root@mail.linuxprobe.com>
2016-10-08 23:38:49 +0800

    参数、解包、变量

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script,first,second,thrid = argv
+
+print "The script is called:", script
+
+print "Your first variable is:", first
+
+print "Your second vairable is:", second
+
+print "Your third variable is:", third

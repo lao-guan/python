db0674e46417b0b297c8fd167e78cf9c3aa8acc9
root <root@mail.linuxprobe.com>
2016-11-20 17:37:11 +0800

    if 语句

---
+++ ex30_1.py
@@ -0,0 +1,14 @@
+people = 30
+cars = 40
+buses = 15
+
+#if cars > people and buses < cars:
+#    print "True."
+if people > cars and buses < cars:
+    print "True."
+else:
+    print "False."
+if people > buses or people < cars:
+    print "True."
+else:
+    print "False."
